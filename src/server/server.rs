caab6c42c4287f976c2ea86af2f47d8ee9c9f5af
Konstantin Ponomarev <vapvapapvpva@yandex.ru>
2026-01-30 01:26:17 +0400

    fix: add rustfmt

--- src/server/server.rs
+++ src/server/server.rs
@@ -12,49 +12,49 @@ pub struct Server {
 }
 
 impl Server {
-    pub fn new(storage: Storage) -> Self {
-        Self { storage }
+    pub fn new(storage: Storage,) -> Self {
+        Self { storage, }
     }
 
-    pub async fn listen(&self) -> Result<(), Box<dyn std::error::Error, >, > {
+    pub async fn listen(&self,) -> Result<(), Box<dyn std::error::Error,>,> {
         let addr = "127.0.0.1:8080";
-        let listener = TcpListener::bind(addr).await?;
-        
-        loop {
-            let (socket, addr, ) = listener.accept().await?;
+        let listener = TcpListener::bind(addr,).await?;
 
-            self.connect(socket, addr);
+        loop {
+            let (socket, addr,) = listener.accept().await?;
+
+            self.connect(socket, addr,);
         }
     }
 
-    pub fn connect(&self, mut socket: TcpStream, _addr: SocketAddr) {
+    pub fn connect(&self, mut socket: TcpStream, _addr: SocketAddr,) {
         let storage = self.storage.clone();
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
 
             loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n, ) if n == 0 => return,
-                    Ok(n, ) => n,
-                    Err(e, ) => {
+                let n = match socket.read(&mut buf,).await {
+                    Ok(n,) if n == 0 => return,
+                    Ok(n,) => n,
+                    Err(e,) => {
                         eprintln!("Error on read: {}", e);
                         return;
                     }
                 };
 
                 let message = storage
-                    .process_input(String::from_utf8_lossy(&buf[..n]).to_string().as_str())
-                    .unwrap_or_else(|err| format!("Error: {err}"));
+                    .process_input(String::from_utf8_lossy(&buf[..n],).to_string().as_str(),)
+                    .unwrap_or_else(|err| format!("Error: {err}"),);
 
-                if let Err(e, ) = socket
-                    .write_all((message + "\r\n").to_string().as_bytes())
+                if let Err(e,) = socket
+                    .write_all((message + "\r\n").to_string().as_bytes(),)
                     .await
                 {
                     eprintln!("Error on write to socket: {}", e);
                     return;
                 }
             }
-        }, );
+        },);
     }
 }
